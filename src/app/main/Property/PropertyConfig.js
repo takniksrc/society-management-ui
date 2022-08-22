bd36ed3c6ec83022070c79f33d35575f2d52a9a8
Arooba-Shabbir <arooba.shabbir@taknik.com>
2022-08-22 15:31:46 +0500

    minor

--- src/app/main/Property/PropertyConfig.js
+++ src/app/main/Property/PropertyConfig.js
@@ -20,7 +20,7 @@ const PropertyConfig = {
 		},
 		{
 			path: '/Settings',
-			component: SettingsComponent	
+			component: SettingsComponent
 		}
 	]
 };
@@ -34,17 +34,17 @@ export default PropertyConfig;
 import React from 'react';
 
 const ExampleConfig = {
-    settings: {
-        layout: {
-            config: {}
-        }
-    },
-    routes  : [
-        {
-            path     : '/example',
-            component: React.lazy(() => import('./Example'))
-        }
-    ]
+	settings: {
+		layout: {
+			config: {}
+		}
+	},
+	routes  : [
+		{
+			path     : '/example',
+			component: React.lazy(() => import('./Example'))
+		}
+	]
 };
 
 export default ExampleConfig;
