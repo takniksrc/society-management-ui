6e7471a0a1dc96bba42d5029dd796be790051d1a
Arooba-Shabbir <arooba.shabbir@taknik.com>
2022-08-22 20:28:34 +0500

    cards outlook

--- src/app/main/Property/Property.js
+++ src/app/main/Property/Property.js
@@ -1,3 +1,4 @@
+import FusePageCarded from "@fuse/core/FusePageCarded";
 import { motion } from "framer-motion";
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -6,7 +7,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import { red } from '@material-ui/core/colors';
-import { Button, Chip, Container } from '@material-ui/core';
+import { Button, Chip, Container, Box } from '@material-ui/core';
 import Avatar from "@material-ui/core/Avatar";
 import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
@@ -14,6 +15,7 @@ import DemoContent from '@fuse/core/DemoContent';
 import FusePageSimple from '@fuse/core/FusePageSimple';
 import { makeStyles } from '@material-ui/core/styles';
 import SettingsComponent from "./Settings";
+
 
 const useStyles = makeStyles({
 	layoutRoot: {}
@@ -35,18 +37,124 @@ function PropertyManagement() {
 			}}
 			header={
 				<div className="px-10 pt-40 font-extrabold">
-					<h4> Property Management </h4>
+					<h3> Property Management </h3>
 				</div>
 			}
 			content={
-				<motion.div className="flex flex-wrap py-24" variants={Container} >
 
-					{customerType.map(item => (
-						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16 border mb-24" 
-						style = {{
-							background: "#F6F9FB"
-						}}>
-							
+				<div className="p-32" >
+					<Box className="p-16 w-full rounded-16 mb-24 border"
+						sx={{
+							backgroundColor: "#F6F9FB"
+						}}
+					>
+						<Typography className="font-medium" variant="h6">Customer Type</Typography>
+						<div className="flex flex-wrap -m-8 mt-8">
+							{customerType.map((item) => (
+								<Box
+									sx={{ backgroundColor: 'background.paper' }}
+									className="relative w-full sm:w-160 h-160 m-8 p-16 shadow rounded-16">
+									<div className="flex shrink flex-col justify-center text-center">
+										<Typography className="truncate text-14 font-medium">{item}</Typography>
+									</div>
+								</Box>
+								
+							))}
+						</div>
+
+					</Box>
+
+					<Box className="p-16 w-full rounded-16 mb-24 border"
+						sx={{
+							backgroundColor: "#F6F9FB"
+						}}
+					>
+						<Typography className="font-medium" variant="h6">Residential Property Type Type</Typography>
+						<div className="flex flex-wrap -m-8 mt-8">
+							{ResidentialPropertyType.map((item) => (
+								<Box
+									sx={{ backgroundColor: 'background.paper' }}
+									className="relative w-full sm:w-160 h-160 m-8 p-16 shadow rounded-16">
+									<div className="flex shrink flex-col justify-center text-center">
+										<Typography className="truncate text-14 font-medium">{item}</Typography>
+									</div>
+								</Box>
+							))}
+						</div>
+
+					</Box>
+
+					<Box className="p-16 w-full rounded-16 mb-24 border"
+						sx={{
+							backgroundColor: "#F6F9FB"
+						}}
+					>
+						<Typography className="font-medium" variant="h6">Commercial Property Type</Typography>
+						<div className="flex flex-wrap -m-8 mt-8">
+							{CommercialPropertyType.map((item) => (
+								<Box
+									sx={{ backgroundColor: 'background.paper' }}
+									className="relative w-full sm:w-160 h-160 m-8 p-16 shadow rounded-16">
+									<div className="flex shrink flex-col justify-center text-center">
+										<Typography className="truncate text-14 font-medium">{item}</Typography>
+									</div>
+								</Box>
+							))}
+						</div>
+
+					</Box>
+
+					<Box className="p-16 w-full rounded-16 mb-24 border"
+						sx={{
+							backgroundColor: "#F6F9FB"
+						}}
+					>
+						<Typography className="font-medium" variant="h6">Residential Property Size</Typography>
+						<div className="flex flex-wrap -m-8 mt-8">
+							{ResidentialPropertySize.map((item) => (
+								<Box
+									sx={{ backgroundColor: 'background.paper' }}
+									className="relative w-full sm:w-160 h-160 m-8 p-16 shadow rounded-16">
+									<div className="flex shrink flex-col justify-center text-center">
+										<Typography className="truncate text-14 font-medium">{item}</Typography>
+									</div>
+								</Box>
+							))}
+						</div>
+
+					</Box>
+
+					<Box className="p-16 w-full rounded-16 mb-24 border"
+						sx={{
+							backgroundColor: "#F6F9FB"
+						}}
+					>
+						<Typography className="font-medium" variant="h6">Commercial Property Size</Typography>
+						<div className="flex flex-wrap -m-8 mt-8">
+							{CommercialPropertySize.map((item) => (
+								<Box
+									sx={{ backgroundColor: 'background.paper' }}
+									className="relative w-full sm:w-160 h-160 m-8 p-16 shadow rounded-16">
+									<div className="flex shrink flex-col justify-center text-center">
+										<Typography className="truncate text-14 font-medium">{item}</Typography>
+									</div>
+								</Box>
+							))}
+						</div>
+
+					</Box>
+
+				</div>
+				/* <motion.div className="flex flex-wrap py-24" variants={Container} >
+				
+
+					{
+					customerType.map(item => (
+						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16 border mb-24"
+							style={{
+								background: "#F6F9FB"
+							}}>
+
 
 							<Card className="flex flex-col h-100 shadow">
 								<div className="flex items-center justify-between mb-16">
@@ -69,7 +177,7 @@ function PropertyManagement() {
 									<Typography variant="h6" component="div">
 										Customer Type
 									</Typography>
-								*/}
+								
 									<Typography className="content-center text-center" variant="subtitle1" >
 										<br />
 										{item}
@@ -79,6 +187,7 @@ function PropertyManagement() {
 						</motion.div>
 					))}
 				</motion.div>
+				*/
 			}
 
 		/>
@@ -88,91 +197,91 @@ function PropertyManagement() {
 
 export default PropertyManagement;
 
-		/*			{ResidentialPropertyType.map(item => (
-						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16">
-							<Card className="flex flex-col h-256 shadow">
-								<div className="flex items-center justify-between mb-16">
-									<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Residential Property Type" variant="h6"
-										style={{
-											backgroundColor: "#d1fae5",
+/*			{ResidentialPropertyType.map(item => (
+				<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16">
+					<Card className="flex flex-col h-256 shadow">
+						<div className="flex items-center justify-between mb-16">
+							<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Residential Property Type" variant="h6"
+								style={{
+									backgroundColor: "#d1fae5",
 
-										}}
-									/>
-								</div>
-								<CardContent>
-
-
-									<Typography variant="subtitle1" className=" justify-center content-center text-center">
-										<br />
-										{item}
-									</Typography>
-								</CardContent>
-							</Card>
-						</motion.div>
-					))}
-
-					{CommercialPropertyType.map(item => (
-						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/2 sm:p-16">
-							<Card className="flex flex-col h-256 shadow">
-								<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Commercial Property Type" variant="h6"
-									style={{
-										backgroundColor: "#d1fae5",
-
-									}}
-								/>
-								<CardContent>
-
-									<Typography className="content-center text-center" variant="subtitle1" >
-										<br />
-										{item}
-									</Typography>
-								</CardContent>
-							</Card>
-						</motion.div>
-					))}
-
-					{ResidentialPropertySize.map(item => (
-						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/5 sm:p-16">
-							<Card className="flex flex-col h-256 shadow">
-								<Chip className="flex content-center px-24  h-40 font-semibold text-center text-lg" label="Residential Property Size" variant="h6"
-									style={{
-										backgroundColor: "#d1fae5",
-
-									}}
-								/>
-								<CardContent>
-
-									<Typography className="content-center text-center" variant="subtitle1" >
-										<br />
-										{item}
-									</Typography>
-								</CardContent>
-							</Card>
-						</motion.div>
-					))}
-
-					{CommercialPropertySize.map(item => (
-						<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16">
-							<Card className="flex flex-col h-256 shadow">
-								<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Commercial Property Size" variant="h6"
-									style={{
-										backgroundColor: "#d1fae5",
-
-									}}
-								/>
-								<CardContent>
-
-									<Typography className="content-center text-center" variant="subtitle1" >
-										<br />
-										{item}
-									</Typography>
-								</CardContent>
-							</Card>
-						</motion.div>
-					))}
+								}}
+							/>
+						</div>
+						<CardContent>
 
 
+							<Typography variant="subtitle1" className=" justify-center content-center text-center">
+								<br />
+								{item}
+							</Typography>
+						</CardContent>
+					</Card>
 				</motion.div>
-								*/
+			))}
+
+			{CommercialPropertyType.map(item => (
+				<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/2 sm:p-16">
+					<Card className="flex flex-col h-256 shadow">
+						<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Commercial Property Type" variant="h6"
+							style={{
+								backgroundColor: "#d1fae5",
+
+							}}
+						/>
+						<CardContent>
+
+							<Typography className="content-center text-center" variant="subtitle1" >
+								<br />
+								{item}
+							</Typography>
+						</CardContent>
+					</Card>
+				</motion.div>
+			))}
+
+			{ResidentialPropertySize.map(item => (
+				<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/5 sm:p-16">
+					<Card className="flex flex-col h-256 shadow">
+						<Chip className="flex content-center px-24  h-40 font-semibold text-center text-lg" label="Residential Property Size" variant="h6"
+							style={{
+								backgroundColor: "#d1fae5",
+
+							}}
+						/>
+						<CardContent>
+
+							<Typography className="content-center text-center" variant="subtitle1" >
+								<br />
+								{item}
+							</Typography>
+						</CardContent>
+					</Card>
+				</motion.div>
+			))}
+
+			{CommercialPropertySize.map(item => (
+				<motion.div className="w-full pb-24 sm:w-1/2 lg:w-1/3 sm:p-16">
+					<Card className="flex flex-col h-256 shadow">
+						<Chip className="flex content-center px-24 w-fit h-40 font-semibold text-center text-lg" label="Commercial Property Size" variant="h6"
+							style={{
+								backgroundColor: "#d1fae5",
+
+							}}
+						/>
+						<CardContent>
+
+							<Typography className="content-center text-center" variant="subtitle1" >
+								<br />
+								{item}
+							</Typography>
+						</CardContent>
+					</Card>
+				</motion.div>
+			))}
+
+
+		</motion.div>
+						*/
 
 
