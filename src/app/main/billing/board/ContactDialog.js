d70bad4317c8e5f658122bf1ea04c66e2ec6eec0
Usman Aziz <usman.aziz@taknik.com>
2022-09-28 18:03:32 +0500

    'Update'

--- src/app/main/billing/board/ContactDialog.js
+++ src/app/main/billing/board/ContactDialog.js
@@ -36,6 +36,7 @@ import { removeUser, closeNewContactDialog, closeEditContactDialog } from '../st
 import { Card } from '@material-ui/core';
 import { forEach } from 'lodash';
 import { showMessage } from 'app/store/fuse/messageSlice';
+import { getBillData } from '../store/billWithIdSlice';
 
 const defaultValuesDiscount = {
 	current_reading: '',
@@ -49,6 +50,8 @@ const defaultValuesPaidAmount = {
 function ContactDialog(props) {
 	const dispatch = useDispatch();
 	const contactDialog = useSelector(({ newUsersSlice }) => newUsersSlice.newUsersSlice);
+	const GetBillsData = useSelector(state => state.scrumboardApp?.billWithIdSlice);
+	console.log('i am GetBills in Dialog', GetBillsData);
 	const [billData, setBillData] = useState({});
 	console.log('I am cliked data ', contactDialog);
 	console.log('billData', billData);
@@ -102,16 +105,44 @@ function ContactDialog(props) {
 		//call the api for data
 		console.log('bill', contactDialog);
 		instance.get(`/api/bills/${contactDialog?.data?.id}`).then(res => setBillData(res.data));
-	}, [contactDialog.data, contactDialog.type, reset]);
-
+	}, [contactDialog.data, contactDialog.type, reset, dispatch]);
+	const onSubmitPaidValuesForm = data => {
+		console.log('data in paid', data);
+		instance
+			.post(`/api/bills/payment/${contactDialog?.data?.id}`, {
+				amount: data.paid_amount
+			})
+			.then(function (response) {
+				console.log('response', JSON.stringify(response.data));
+				dispatch(
+					showMessage({
+						message: response.message, //text or html
+						autoHideDuration: 6000, //ms
+						anchorOrigin: {
+							vertical: 'top', //top bottom
+							horizontal: 'right' //left center right
+						},
+						variant: 'success' //success error info warning null
+					})
+				);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	};
+	const UpdateList =()=>{
+		dispatch(getBillData(contactDialog.data.id))
+	}
 	/**
 	 * On Dialog Open
 	 */
 	useEffect(() => {
+		console.log('I am Ge');
 		if (contactDialog.props.open) {
 			initDialog();
 		}
-	}, [contactDialog.props.open, initDialog]);
+		// dispatch(getBillData(contactDialog?.data?.id));
+	}, [contactDialog.props.open, initDialog, contactDialog.data, dispatch]);
 
 	function closeComposeDialog() {
 		return contactDialog.type === 'edit' ? dispatch(closeEditContactDialog()) : dispatch(closeNewContactDialog());
@@ -142,30 +173,7 @@ function ContactDialog(props) {
 				console.log(error);
 			});
 	};
-	const onSubmitPaidValuesForm = data => {
-		console.log('data in paid', data);
-		instance
-			.post(`/api/bills/payment/${contactDialog?.data?.id}`, {
-				amount: data.paid_amount
-			})
-			.then(function (response) {
-				console.log('response', JSON.stringify(response.data));
-				dispatch(
-					showMessage({
-						message: response.message, //text or html
-						autoHideDuration: 6000, //ms
-						anchorOrigin: {
-							vertical: 'top', //top bottom
-							horizontal: 'right' //left center right
-						},
-						variant: 'success' //success error info warning null
-					})
-				);
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	};
+	
 
 	return (
 		<Dialog
@@ -197,7 +205,7 @@ function ContactDialog(props) {
 
 			<DialogContent classes={{ root: 'p-24' }}>
 				<div className="flex gap-36">
-					<Card className="w-3/5" style={{ padding: '2rem' }}>
+					{/* <Card className="w-3/5" style={{ padding: '2rem' }}>
 						<TableContainer>
 							<Table aria-label="customized table">
 								<TableHead>
@@ -306,22 +314,52 @@ function ContactDialog(props) {
 								</TableBody>
 							</Table>
 						</TableContainer>
-					</Card>
-
-					<div className="flex w-2/5	 flex-col gap-24">
-						<Card className="w-full" style={{ padding: '2rem' }}>
-							<Typography color="error">Due Date: {contactDialog?.data?.due_date}</Typography>
-							<Typography>
-								Units: {contactDialog?.data?.current_reading - contactDialog?.data?.previous_reading}{' '}
-							</Typography>
-							<Typography>Previous Reading: {contactDialog.data?.previous_reading} </Typography>
-							<Typography>Arrears: {contactDialog.data?.arrears}</Typography>
-							<Typography>Total FPA: {contactDialog.data?.fpa_charges}</Typography>
-							<Typography>Current Bill: {contactDialog.data?.electricity_charges}</Typography>
-							<Typography>Society Charges: {contactDialog.data?.society_charges}</Typography>
-							<Typography>Total Payables: {contactDialog.data?.total_bill} </Typography>
+					</Card> */}
+					<form className="w-full" noValidate onSubmit={handleSubmitPaidValues(onSubmitPaidValuesForm)}>
+						<Card className="p-24">
+							<div className="flex">
+								<Controller
+									control={controlPaidValues}
+									name="paid_amount"
+									render={({ field }) => (
+										<TextField
+											{...field}
+											type="number"
+											className="mb-24"
+											label="Paid Amount"
+											id="discont"
+											variant="outlined"
+											fullWidth
+										/>
+									)}
+								/>
+							</div>
+							<div className="px-16">
+								<Button
+									variant="contained"
+									color="secondary"
+									type="submit"
+									onClick ={UpdateList}
+									// onClick={() => dispatch(getBillData(contactDialog.data.id))}
+									// disabled={_.isEmpty(dirtyFields) || !isValid}
+								>
+									Add Payment
+								</Button>
+							</div>
+							<div>
+								{GetBillsData?.payment_history?.map((item, index) => {
+									return (
+										<Card className="p-10 m-10">
+										<Typography>
+											<b>Paid PKR</b> {item?.amount} , Date {item?.payment_date}
+										</Typography>
+										</Card>
+									);
+								})}
+							</div>
 						</Card>
-
+					</form>
+					<div className="flex w-2/5	 flex-col gap-24">
 						<form className="w-full" noValidate onSubmit={handleSubmit(onSubmitDiscountValuesForm)}>
 							<Card className="p-24">
 								<div>
@@ -371,37 +409,18 @@ function ContactDialog(props) {
 								</div>
 							</Card>
 						</form>
-						<form className="w-full" noValidate onSubmit={handleSubmitPaidValues(onSubmitPaidValuesForm)}>
-							<Card className="p-24">
-								<div className="flex">
-									<Controller
-										control={controlPaidValues}
-										name="paid_amount"
-										render={({ field }) => (
-											<TextField
-												{...field}
-												type="number"
-												className="mb-24"
-												label="Paid Amount"
-												id="discont"
-												variant="outlined"
-												fullWidth
-											/>
-										)}
-									/>
-								</div>
-								<div className="px-16">
-									<Button
-										variant="contained"
-										color="secondary"
-										type="submit"
-										// disabled={_.isEmpty(dirtyFields) || !isValid}
-									>
-										Add Payment
-									</Button>
-								</div>
-							</Card>
-						</form>
+						<Card className="w-full" style={{ padding: '2rem' }}>
+							<Typography color="error">Due Date: {contactDialog?.data?.due_date}</Typography>
+							<Typography>
+								Units: {contactDialog?.data?.current_reading - contactDialog?.data?.previous_reading}{' '}
+							</Typography>
+							<Typography>Previous Reading: {contactDialog.data?.previous_reading} </Typography>
+							<Typography>Arrears: {contactDialog.data?.arrears}</Typography>
+							<Typography>Total FPA: {contactDialog.data?.fpa_charges}</Typography>
+							<Typography>Current Bill: {contactDialog.data?.electricity_charges}</Typography>
+							<Typography>Society Charges: {contactDialog.data?.society_charges}</Typography>
+							<Typography>Total Payables: {contactDialog.data?.total_bill} </Typography>
+						</Card>
 					</div>
 				</div>
 			</DialogContent>
